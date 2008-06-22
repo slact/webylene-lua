80d58a768c1e2b417115f343487e4791c2579982
Leo P <junk@slact.net>
2008-06-22 22:10:37 +0000

    temporarily with some debugstuff

--- objects/core/database.lua
+++ objects/core/database.lua
@@ -1,10 +1,13 @@
 require "luasql.mysql"
+require "gettimeofday" -- temp needed for debugging
+
+--- the database talker object. also available as webylene.db
 database = {
 	init = function(self)
 		webylene.db = self -- shorthand for everyone else to use.
 		
 		event:addAfterListener("loadConfig",function()
-			self.settings=cf("database")
+			self.settings=cf("database") -- these will be loaded by now.
 			if not self.settings then return nil, "no database config. ain't even gonna try to connect" end
 			self.env = luasql.mysql()
 			--connect
@@ -19,17 +22,34 @@ database = {
 		end)
 	end,
 	
+	--- perform an SQL query. returns a cursor for SELECT queries, number of rows touched for all other queries,(nil, error) on error.
+	-- @param str query
 	query = function(self, str)
-		return self.conn:execute(str)
+		local start = os.gettimeofday()
+		local res, err = self.conn:execute(str)
+		local finish = os.gettimeofday()
+		if (finish - start) > 0.05 then
+			logger:info("Unusually long query: '" .. str .. "' took " .. (finish - start) .. "msec")
+		end
+		if res == nil then
+			return nil, err .. ". QUERY WAS: " .. str
+		end
+		return res
 	end,
 	
+	--- for loop iterator for a database result cursor. see the luaSQL cursor documentation
 	rows = function(self, cur, mode)
+		if not cur then
+			return nil, "no cursor provided."
+		end
 		local mode = mode or 'a'
 		return function()
 			return cur:fetch({}, mode)
 		end
 	end,
 	
+	--- returns first row of query result cursor.
+	-- behavior undefined if cur isn't a valid database result cursor
 	firstRow = function(self, cur, mode)
 		local mode = mode or 'a'
 		local res = cur:fetch({}, mode)
@@ -37,6 +57,7 @@ database = {
 		return res
 	end,
 	
+	--- perform query, return results as a table.
 	niceQuery = function(self, str)
 		local cur, err = self.conn:execute(str)
 		if not cur then
@@ -50,14 +71,18 @@ database = {
 		return rows
 	end,
 	
+	--- escape quotes n' such to avoid an SQL injection
+	-- @param str string to escape
 	esc = function(self, str)
 		return self.conn:escape(str)
 	end,
 	
+	--- format a time to sql-standard date format
 	date = function(self, time)
 		return os.date("%Y-%m-%d %X",time)
 	end,
 	
+	--- asks the database what time it is. 
 	now = function(self)
 		local res = db:firstRow(db:query("SELECT NOW() as now;"))
 		return res.now
