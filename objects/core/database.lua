d972fc58854d1c73a7a1458bb3fb4b5b2e9adf2f
Leo P <junk@slact.net>
2009-07-07 01:26:19 +0000

    more general databaseinterfacer. not thoroughly tested yet.

--- objects/core/database.lua
+++ objects/core/database.lua
@@ -2,8 +2,33 @@ local webylene, event = webylene, event
 
 local db_mt --closured
 
---- the database class.
+--- the database class. uses LuaSQL to talk to databases.
+--- see http://www.keplerproject.org/luasql/manual.html for more info.
+--- LuaSQL (as of version 2.1.1) supports ODBC, ADO, Oracle, MySQL, SQLite, and PostgreSQL.
 --- database connection will be available as webylene.db
+
+--- EVENTS:
+--[[ during initialization:
+	<initialize source="core">
+		access database-related config
+		if using persistent connections, connect to database
+	</initialize>
+]]
+--[[ during a request:
+	<request source="core">
+		<databaseTransaction>
+			on start: if not using persistent connections, connect to db
+	
+			on finish: if not using persistent connections, disconnect from db
+		</databaseTransaction>
+	</request>
+]]
+--[[
+	<shutdown source="core">
+		close all active database connections
+	</shutdown>
+]]
+
 database = {
 	--- initialize db connection
 	init = function(self)
@@ -15,48 +40,51 @@ database = {
 		event:addListener("initialize",function()
 			db_settings=cf("database") -- these will be loaded by now.
 			if not db_settings then return nil, "no database config. ain't even gonna try to connect" end
+			if db_settings.disabled==true or db_settings.enabled==false then
+				
+			end
 			db_instance = assert(self:new(db_settings.type))
 			webylene.db = db_instance --- ############ Let this not slip by thine eyes ############
-		end)
-		if db_settings.persist==true or db_settings.persist == "true" then
-			assert(db_instance:connect(db_settings.db, db_settings.username, db_settings.password, db_settings.host, db_settings.port or 3306))
-			event:addStartListener("request", function()
-				if not db_instance:connected() then assert(db_instance:connect(db_settings.db, db_settings.username, db_settings.password, db_settings.host, db_settings.port or 3306)) end
-				event:start("databaseTransaction")
-			end)
-			
-			event:addFinishListener("request", function()
-				event:finish("databaseTransaction")
-				--disconnect
-			end)
-			
-			event:addListener("shutdown", function()
-				if db_instance:connected() then 
-					db_instance:close()
-					--db_instance = nil --no, keep these around
-					--webylene.db = nil
-				end
-			end)
-		else
-			event:addStartListener("request", function()
-				--connect
+			if db_settings.persist==true or db_settings.persist == "true" then
 				assert(db_instance:connect(db_settings.db, db_settings.username, db_settings.password, db_settings.host, db_settings.port or 3306))
-				event:start("databaseTransaction")
-			end)
-			
-			event:addFinishListener("request", function()
-				event:finish("databaseTransaction")
-				--disconnect
-				if db_instance:connected() then 
-					db_instance:close()
-					--db_instance = nil --no, keep these around
-					--webylene.db = nil
-				end
-			end)
-		end
+				event:addStartListener("request", function()
+					if not db_instance:connected() then assert(db_instance:connect(db_settings.db, db_settings.username, db_settings.password, db_settings.host, db_settings.port or 3306)) end
+					event:start("databaseTransaction")
+				end)
+				
+				event:addFinishListener("request", function()
+					event:finish("databaseTransaction")
+					--disconnect
+				end)
+				
+				event:addListener("shutdown", function()
+					if db_instance:connected() then 
+						db_instance:close()
+						--db_instance = nil --no, keep these around
+						--webylene.db = nil
+					end
+				end)
+			else
+				event:addStartListener("request", function()
+					--connect
+					assert(db_instance:connect(db_settings.db, db_settings.username, db_settings.password, db_settings.host, db_settings.port or 3306))
+					event:start("databaseTransaction")
+				end)
+				
+				event:addFinishListener("request", function()
+					event:finish("databaseTransaction")
+					--disconnect
+					if db_instance:connected() then 
+						db_instance:close()
+						--db_instance = nil --no, keep these around
+						--webylene.db = nil
+					end
+				end)
+			end
+		end)
 	end,
 	
-	--- new db connection object maybe? (doesn't actually connect, that happens later.
+	--- new db connection object maybe? (doesn't actually connect, that happens a little later.)
 	new = function(self, database_type) 
 		if not database_type then 
 			return nil, "no database type specified..."
@@ -66,7 +94,10 @@ database = {
 		local luasql_dbtype = string.format("luasql.%s", database_type)
 		require(luasql_dbtype)
 		local env = luasql[database_type]()
+		
+		--here's some of the beef of a database object.
 		return setmetatable({
+			
 			connect = function(self, ...)
 				self.connected = function(self)
 					return self.connection and true
@@ -82,6 +113,7 @@ database = {
 				if not self.connection then return nil, err end
 				return self 
 			end,
+			
 			close = function(self)
 				if self.connection:close() then
 					self.connection = nil
@@ -90,7 +122,8 @@ database = {
 					return nil, "can't close connection -- it's still being used."
 				end
 			end,	
-			query = db_mt.__index.query -- speed hack. is it desired, or just ugly?
+			query = db_mt.__index.query, -- speed hack. is it desired, or just ugly?
+			driver = database_type:lower() --may come in handy
 		}, db_mt)
 	end	
 }
@@ -129,14 +162,14 @@ db_mt = { __index = {
 	-- @param cur database query cursor. undefined behavior if cur is anything else.
 	-- @return table of resulting rows or nil, err on error
 	results = function(self, cur)
-		local res = {}
+		local res, tins = {}, table.insert
 		for row in self:rows(cur) do
-			table.insert(res, row)
+			tins(res, row)
 		end
 		return res
 	end,	
 	
-	--- returns first row of query result cursor.
+	--- returns first row of query result cursor. closes the curson upon completion.
 	-- behavior undefined if cur isn't a valid database result cursor
 	firstRow = function(self, cur, mode)
 		local mode = mode or 'a'
@@ -148,16 +181,9 @@ db_mt = { __index = {
 	--- perform query, return results as a table.
 	--@return table of resultant rows, or nil, err_msg on error
 	niceQuery = function(self, str)
-		local cur, err = self.connection:execute(str)
-		if not cur then
-			return cur, err
-		end
-		local rows = {}
-		for row in self:rows(cur) do
-			table.insert(rows, row)
-		end
-		cur:close()
-		return rows
+		local cur, err = self:query(str)
+		if not cur then return nil, err end
+		return self:results(cur)
 	end,
 	
 	--- escape quotes n' such to avoid an SQL injection
@@ -177,17 +203,48 @@ db_mt = { __index = {
 		if not datestring or datestring == "" then 
 			return nil 
 		end
-		return tonumber(self:firstRow(self:query("SELECT UNIX_TIMESTAMP('" .. self:esc(datestring).. "') as stamp;"))["stamp"])
+		local unixtime_function, dbtype = "", self.driver
+		if dbtype=="mysql" then
+			unixtime_function = "UNIX_TIMESTAMP('%s')"
+		elseif dbtype == "sqlite" then -- EXTREME DANGER: extremely untested.
+			unixtime_function = "strftime('%%s', '%s')"
+		elseif dbtype == "oracle" then -- EXTREME DANGER: extremely untested.
+			unixtime_function= "DATEDIFF(s, '19700101', '%s')"
+		elseif dbtype == "odbc" or dbtype=="ado" then
+			return nil, "not yet implemented"
+		end
+		return tonumber(self:firstRow(self:query("SELECT " .. unixtime_function:format(self:esc(datestring)) .. " as stamp;"))["stamp"])
 	end,
 	
-	--- asks the database what time it is. 
+	--- asks the database what time it is
+	-- @return database-native timestamp string
 	now = function(self)
-		return self:firstRow(self:query("SELECT NOW() as now;"))["now"]
+		local fnow, dbtype = "", self.driver
+		if dbtype=="mysql" then
+			fnow = "NOW()"
+		elseif dbtype == "sqlite" then -- mild danger: not terribly well-tested
+			fnow = "date('now')"
+		elseif dbtype == "oracle" then -- Notable Danger: not well-tested
+			fnow= "CURRENT_DATE"
+		elseif dbtype == "odbc" or dbtype=="ado" then
+			return nil, "not yet implemented"
+		end
+		return self:firstRow(self:query("SELECT " .. fnow .. " as now;"))["now"]
 	end,
 	
 	--- asks the database what Unix-time it is. 
 	unix_timestamp = function(self)
-		return tonumber(self:firstRow(self:query("SELECT UNIX_TIMESTAMP() as now;"))["now"])
+	local fnow, dbtype = "", self.driver
+		if dbtype=="mysql" then
+			fnow = "UNIX_TIMESTAMP()"
+		elseif dbtype == "sqlite" then -- mild danger: not terribly well-tested
+			fnow = "strftime('%s','now')"
+		elseif dbtype == "oracle" then -- EXTREME DANGER: extremely untested.
+			fnow="DATEDIFF(s, '19700101', CURRENT_DATE)"
+		elseif dbtype == "odbc" or dbtype=="ado" then
+			return nil, "not yet implemented"
+		end
+		return tonumber(self:firstRow(self:query("SELECT " .. fnow .. " as now;"))["now"])
 	end,
 	
 	commit = function(self)
