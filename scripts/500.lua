424f10b13ab59999f86aca1c65374c32caf0b420
Leo P <junk@slact.net>
2009-08-05 03:19:29 +0000

    nicer error templates

--- scripts/500.lua
+++ scripts/500.lua
@@ -1,2 +1,9 @@
 response.status = "500 Internal Server Error"
-template:out("500", {error = request.params.error, trace = request.params.trace})
+
+if string.lower(response['content-type'] or "") == 'text/plain' then
+	print("An error occurred while processing this request")
+	print(request.params.error or "")
+	print(request.params.trace or "")
+else
+	template:out("500", {error = request.params.error, trace = request.params.trace})
+end
